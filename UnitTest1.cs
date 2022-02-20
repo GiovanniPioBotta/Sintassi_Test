87cc5cb334fb8c2b350e7f3e482a9da8a4b5107a
GiovanniPioBotta <58598471+GiovanniPioBotta@users.noreply.github.com>
2022-02-20 18:50:59 +0100

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test_GB
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange (Inizializzo le variabili e il valore atteso)
+            var x = 5;
+            var y = 5;
+            var expected = 25;
+
+            // Act (Inizializzo il valore effettivo)
+            var actual = x * y;
+
+            // Assert (Confronto valore atteso e valore effettivo)
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
